3aa1aca5c47e7bfe666722c82c86f180806608aa
beni-f <benifissha@gmail.com>
2024-06-25 16:13:10 +0300

    fifo_cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+    FIFO caching
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+    
+    def put(self, key, item):
+        """
+            Add cache data using the FIFO algorithm
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if (len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data):
+                for k, v in self.cache_data.items():
+                    discard_key = k
+                    discard_item = v
+                    break
+                print(f"DISCARD: {discard_key}")
+                del self.cache_data[discard_key]
+            self.cache_data[key] = item
+
+
+    def get(self, key):
+        """
+            Return the value in self.cache_data linked to key. 
+        """
+        if key in self.cache_data and self.cache_data[key]:
+            return self.cache_data[key]
+        else:
+            return None
