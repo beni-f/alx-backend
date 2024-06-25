4237980b16016629a3ca1a5b7a57edb14f5d883a
beni-f <benifissha@gmail.com>
2024-06-25 14:44:48 +0300

    0-basic_cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+    Basic dictionary
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """
+            Must assign to the dictionary self.cache_data the
+            item value for the key 'key'
+        """
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            Must return the value in self.cache_data linked to key.
+        """
+        if key in self.cache_data and self.cache_data[key]:
+            return self.cache_data[key]
+        else:
+            return None
