ab381c56920eea3dd95502abfd3b5f946ba63811
beni-f <benifissha@gmail.com>
2024-07-02 21:30:25 +0300

    2-app

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Get locale from request
+"""
+from flask_babel import Babel
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+        Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localselector
+def get_locale():
+    """
+        Returns the best choice of language
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+        Simple route
+    """
+    return render_template('2-index.html')
