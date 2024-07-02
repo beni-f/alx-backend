5131713fd951a84980da4a3c4cfbb1eff37df42a
beni-f <benifissha@gmail.com>
2024-07-02 21:12:50 +0300

    0-app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ 
+        Simple route
+    """
     return render_template('0-index.html')
