8348ea6fb553ad41816407ef8dafbb7e50dc43fd
beni-f <benifissha@gmail.com>
2024-07-02 21:53:29 +0300

    updated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,9 +5,10 @@ Basic Flask app
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    """ 
+    """
         Simple route
     """
     return render_template('0-index.html')
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,20 +5,26 @@ Basic Babel setup
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 
 
 class Config:
+    """
+        Config Class
+    """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE ='en'
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @app.route('/')
 def index():
-    """ 
+    """
         Simple route
     """
     return render_template('1-index.html')
